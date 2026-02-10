611276761c1b6c23b2221a7d9c52fdf5c74c76c9
Francisco Gomes <fgsj.dev@outlook.com>
2026-02-10 20:42:36 -0300

    Update file with two exercises

--- 3 - Clausule-SUM.sql
+++ 3 - Clausule-SUM.sql
@@ -10,3 +10,11 @@ Version: 1.0
 -- Exercise 01: Some o faturamento total bruto de todos os pedidos (SubTotal).
 SELECT SUM(soh.SubTotal) as Total_Bruto_Pedido
 FROM SalesLT.SalesOrderHeader as soh
+
+-- Exercise 02: Calcule o valor total de todos os produtos em estoque na tabela Product.
+SELECT SUM(p.ListPrice) as Preco_Lista_Final
+FROM SalesLT.[Product] as p
+
+-- Exercise 03: Some o valor total de todas as vendas realizadas na tabela SalesOrderHeader.
+SELECT SUM(soh.TotalDue) as Total_Final
+FROM SalesLT.SalesOrderHeader as soh
